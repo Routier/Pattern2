5ab5156e5e72d5a0650bdc846cef5d25feb78a61
num13 <num13-ne@mail.ru>
2024-07-03 20:10:17 +0300

    BeforeAll to BeforeEach

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,5 +1,5 @@
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -8,16 +8,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class LoginTest {
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         open("http://localhost:9999");
     }
 
     @Test
     void successLogin() {
-        var trueUser = DataGen.Registration.getRegisteredUser("active");
-        $("[data-test-id='login'] input").setValue(trueUser.getLogin());
-        $("[data-test-id='password'] input").setValue(trueUser.getPassword());
+        var registeredUser = DataGen.Registration.getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
         $("button.button").click();
         $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
     }
@@ -29,32 +29,32 @@ public class LoginTest {
         $("[data-test-id='password'] input").setValue(falseUser.getPassword());
         $("button.button").click();
         $("[data-test-id='error-notification'] .notification__content").shouldHave(Condition.text("Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
-    }
+   }
 
     @Test
     void blockedUserFailLogin() {
         var blockedUser = DataGen.Registration.getRegisteredUser("blocked");
         $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
-        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+       $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
         $("button.button").click();
         $("[data-test-id='error-notification'] .notification__content").shouldHave(Condition.text("Пользователь заблокирован"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
     }
 
     @Test
     void failLoginWrongLogin() {
-        var registeredUser = DataGen.Registration.getRegisteredUser("active");
+        var registeredUser2 = DataGen.Registration.getRegisteredUser("active");
         var wrongLogin = DataGen.getRandomLogin();
         $("[data-test-id='login'] input").setValue(wrongLogin);
-        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='password'] input").setValue(registeredUser2.getPassword());
         $("button.button").click();
         $("[data-test-id='error-notification'] .notification__content").shouldHave(Condition.text("Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
     }
 
     @Test
     void failLoginWrongPassword() {
-        var registeredUser = DataGen.Registration.getRegisteredUser("active");
+        var registeredUser3 = DataGen.Registration.getRegisteredUser("active");
         var wrongPassword = DataGen.getRandomPassword();
-        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='login'] input").setValue(registeredUser3.getLogin());
         $("[data-test-id='password'] input").setValue(wrongPassword);
         $("button.button").click();
         $("[data-test-id='error-notification'] .notification__content").shouldHave(Condition.text("Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
